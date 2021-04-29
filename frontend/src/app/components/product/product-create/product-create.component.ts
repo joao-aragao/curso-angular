c825daf788fecbc22dd4f21be4ec0d436e532982
Joao Aragao <joaovictoraragaoads@gmail.com>
2021-04-29 15:01:27 -0300

    request post product

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -9,6 +10,11 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: null
+  }
+
   constructor(private productService: ProductService,
     private router: Router) { }
 
@@ -17,6 +23,9 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.router.navigate(['/products'])
+    });
     this.productService.showMenssage('Produto Cadastrado com Sucesso!');
   }
 
